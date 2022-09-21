9a8dc0d0cce89ed6dbbe9a0eacbea1b9a7535a3e
sktp06 <kantaporn_s@cmu.ac.th>
2022-09-22 01:27:34 +0700

    1.1

--- src/main/java/se331/rest/entity/Event.java
+++ src/main/java/se331/rest/entity/Event.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 @Data
 @Builder
@@ -25,7 +26,8 @@ public class Event {
     String date;
     String time;
     Boolean petAllowed;
-    String organizer;
+    Organizer organizer;
+    List<Participant> participants;
 }
 
 
---
+++ src/main/java/se331/rest/entity/Participant.java
@@ -0,0 +1,10 @@
+package se331.rest.entity;
+
+import java.util.List;
+
+public class Participant {
+    Long id;
+    String name;
+    String telNo;
+    List<Event> eventHistory;
+}
